62cedfe684cad8d355e046bc1bfb2228e1bdcb22
Sergio Ribeiro <sergiorcerqueira@gmail.com>
2024-09-14 08:00:00 -0300

    Acertos de estatísticas de tabelas e campos

--- src/eda_analisys.py
+++ src/eda_analisys.py
@@ -12,6 +12,8 @@ from config import *
 from config import log_config
 import numpy as np
 import csv
+import math
+import re
 
 logger = log_config()
 
@@ -105,17 +107,23 @@ def pk_unique(table:str,df_fields: DataFrame, file_path:str,sep:str):
             result = 'no'
     return result
 
-def field_values(table:str,field:str,field_type:str, file_path:str,sep:str):
+def field_values(table:str, field:str, regex_value:str, values_list: str, ranges_list: str, file_path:str, sep:str):
+    # validations flags
+    flg_format = (not pd.isna(regex_value)) and bool(str(regex_value).strip())
+    flg_list = (pd.isna(values_list) is False) and bool(str(values_list).strip())
+    flg_range = (pd.isna(ranges_list) is False) and bool(str(ranges_list).strip())
+    # data read
     encode_search = from_path(file_path).best() 
     encode = encode_search.encoding       
     df_dados = pd.read_csv(file_path,encoding=encode,sep=sep,engine='python')
     df_dados.columns = df_dados.columns.str.lower() 
+    field_series = df_dados[field]
+    # values collect
+    high = field_series.max() 
+    low = field_series.min() 
+    qty_emptys, qty_nulls, qty_zeroes, qty_negatives, qty_uniques, qty_formats, qty_list, qty_range = 0, 0, 0, 0, 0, 0, 0, 0   
     total_records = len(df_dados)
-    qty_emptys, qty_nulls, qty_zeroes, qty_negatives = 0, 0, 0, 0 
-    series = df_dados[field]
-    high = series.max() 
-    low = series.min()     
-    for idx, vlr in series.items():
+    for idx, vlr in field_series.items():
         if pd.isna(vlr):
             qty_nulls += 1
             continue
@@ -130,14 +138,38 @@ def field_values(table:str,field:str,field_type:str, file_path:str,sep:str):
             elif vlr_num < 0: 
                 qty_negatives += 1       
         except ValueError:
-            pass              
+            pass      
+    qty_uniques = df_dados[field].nunique()
+    if flg_format: 
+        qty_formats = df_dados[field].astype(str).str.match(regex_value, na=False).sum()
+    if flg_list:
+        values_lst = values_list.split(';') 
+        qty_list = df_dados[field].astype(str).isin(values_lst).sum()
+    if flg_range: 
+        range_lst = ranges_list.split(';') 
+        min_limit, max_limit = range_lst[0], range_lst[1]
+        qty_range = ((df_dados[field].astype(float) >= float(min_limit)) & (df_dados[field].astype(float) <= float(max_limit))).sum()
+
     print("------------------- Field ---------------")
     print(field)
     print(total_records, qty_emptys, qty_nulls, qty_zeroes, qty_negatives)
-    perc_emptys = round((qty_emptys / total_records),6) * 100
-    perc_nulls = round((qty_nulls / total_records),6) * 100
-    perc_zeroes = round((qty_zeroes / total_records),6) * 100
-    perc_negatives = round((qty_negatives / total_records),6) * 100
+    print(flg_list)
+    print(values_list)
+    print(flg_range)
+    print(ranges_list)
+    # percent calc
+    perc_emptys = round(((qty_emptys / total_records) * 100),2)
+    perc_nulls = round(((qty_nulls / total_records) * 100),2)
+    perc_zeroes = round(((qty_zeroes / total_records) * 100),2)
+    perc_negatives = round(((qty_negatives / total_records) * 100),2)
+    perc_uniques  = round(((qty_uniques / total_records) * 100),2)
+    if  flg_format: 
+        perc_formats  = round(((qty_formats / total_records) * 100),2)
+    if  flg_list: 
+        perc_list  = round(((qty_list / total_records) * 100),2)  
+    if  flg_range: 
+        perc_range  = round(((qty_range / total_records) * 100),2)          
+    # return format
     if qty_emptys > 0: 
         ret_emptys = f"{qty_emptys} ({perc_emptys}%)"
     else: 
@@ -154,7 +186,20 @@ def field_values(table:str,field:str,field_type:str, file_path:str,sep:str):
         ret_negatives = f"{qty_negatives} ({perc_negatives}%)"
     else: 
         ret_negatives = 0
-    return low, high, ret_emptys, ret_nulls, ret_zeroes, ret_negatives
-
+    ret_uniques =  perc_uniques
+    if flg_format: 
+        ret_formats =  f"{qty_formats} ({perc_formats}%)"
+    else: 
+        ret_formats = "no format"
+    if flg_list: 
+        ret_list =  f"{qty_list} ({perc_list}%)"
+    else: 
+        ret_list = "no list"
+    if flg_range: 
+        ret_range =  f"{qty_range} ({perc_range}%)"
+    else: 
+        ret_range = "no range"
+    # return
+    return low, high, ret_emptys, ret_nulls, ret_zeroes, ret_negatives, ret_uniques, ret_formats, ret_list, ret_range
 
 
